77354e00345f5334a4ae0636240bd8c9a319a72e
violetacastro <violetacastrogutierrez@gmail.com>
2020-05-21 19:04:47 +0000

    add terms and conditions static page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get 'help' => "static_pages#help"
   get 'about' => "static_pages#about"
   get 'contact' => "static_pages#contact"
+  get "terms" => "static_pages#terms"
 end
