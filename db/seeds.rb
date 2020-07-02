15ffca955d0514b670462d4932aa7c5ddad282ac
violetacastro <violetacastrogutierrez@gmail.com>
2020-07-02 20:32:48 +0000

    add more posts to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,3 +39,21 @@ Post.create!(title: "The Kremlin Fears American Disorder More Than It Celebrates
 There has indeed been some of that. But far from delivering any unified message to the world or to its domestic audience, Russian state media, through some of its popular television talk shows, has been airing debates about Black Lives Matter that betray a great deal about how the Kremlin views itself and the fragility of state power.",
              user_id: user_one.id
 )
+
+
+Post.create!(title: "Five Lessons To Remember When Lockdown Ends",
+             content: "(ychologist Jill Suttie explores what she and so many of us have felt and learned during the pandemic -- and the growing discussion about what comes next. Her own experience living in a city that imposed shelter-in-place orders 10 weeks ago as of this writing has been a mixed bag. I shifted to working at home pretty easily but it’s been hard finding a routine and avoiding distractions. I’m connected with friends online but I miss their physical presence. Plus my sleep and mood have suffered as anxiety looms over the future of our society. I don’t want to negate these feelings or ignore our losses. Suttie offers five lessons she wants to hold on to once sheltering in place is lifted like being with others is key to happiness. Reducing stress is good for everyone. Showing gratitude matters. We need less stuff that we think. And we are stronger when we act together.)",
+             user_id: user_one.id
+)
+
+
+Post.create!(title: "World’s Rarest Wading Bird is Making a Comeback as Its Population Rises By 30%",
+             content: "A decades-long fight to recover the world’s rarest wading bird is turning a corner with the success of New Zealand’s massive effort to prevent extinction of their most unique and amazing species.",
+             user_id: user_two.id
+)
+
+
+Post.create!(title: "The Hong Kong crisis and the new world order",
+             content: "With the UK opening its doors to three million Hong Kong residents and China threatening serious retaliation for what it sees as an intrusion into its domestic affairs, the Hong Kong crisis is becoming a real-time test of diplomacy in a pandemic-distracted world.",
+             user_id: user_one.id
+)
